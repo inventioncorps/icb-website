395d3987aac0f7cef32318f72738964a3f3a419d
Courtnie Chan <courtnie@berkeley.edu>
2020-04-22 12:52:10 -0700

    added nav bkd color on scroll

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Make nav background white instead of transparent after scrolling
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+
+// Update nav to show where you are (not working rn)
+$(document).ready(function () {
+    var url = window.location;
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+         return this.href == url;
+    }).parent().addClass('active');
+});
